3567592c809511337006e2586a49addf9bfa6098
Don Pinkus <don.pinkus@gmail.com>
2014-10-27 11:49:54 -0700

    front-end vote and comment form

--- app/assets/javascripts/topics_index.js
+++ app/assets/javascripts/topics_index.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
-  $(".topicContainer").click(function(){
-    var link = $(this).find('.topicLink').attr('href');
-    window.location = link;
+  // $(".topicContainer").click(function(){
+  //   var link = $(this).find('.topicLink').attr('href');
+  //   window.location = link;
+  // });
+  $('.topicActions .btn').click(function(){
+    // Show form
+    $(this).closest('.topicContainer').find('.commentForm').removeClass('hidden');
   });
 });
